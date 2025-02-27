891c4fb3de039f6e46782ab522733984c9002bf7
doche15 <dominik.chevalier.1@ulaval.ca>
2025-02-26 22:42:11 -0500

    Proposition simplification

--- pdf_MPMRF.R
+++ pdf_MPMRF.R
@@ -2,7 +2,7 @@
 ### Travail 2, ACT-7119
 ### Theorem 3 : Joint Probability Mass Function.
 ###
-
+library(igraph)
 source("find_parent_of_v.R")
 
 pdf_MPMRF = function(A, lambda, x_vec, root_node){
@@ -10,42 +10,45 @@ pdf_MPMRF = function(A, lambda, x_vec, root_node){
   # lambda : paramètre des lois de Poisson
   # x_vec : (x_1, ..., x_d)
   # root_node : numéro de la racine
-  
+
   d = nrow(A)
-  
+
   v_no_root = seq(d)[-root_node]
-  
+
   prod_vec = numeric(d - 1)
-  
+
   i = 0
   for (v in v_no_root){
-    
+
     i = i + 1 # stock prod_vec
-    
+
     # parent de v
     pav = find_parent_of_v(A, v, root_node)
-    
+
     sum_v = 0
-    
+
     for (k in seq(0, min(x_vec[pav], x_vec[v]))){
-      
+
       sum_v = sum_v +
-      (exp(-lambda * (1 - A[pav, v])) * 
+      (exp(-lambda * (1 - A[pav, v])) *
         (lambda * (1 - A[pav, v]))^(x_vec[v] - k)) /
         factorial(x_vec[v] - k) *
         choose(x_vec[pav], k) *
         A[pav, v]^k *
         (1 - A[pav, v])^(x_vec[pav] - k)
-        
+
+      ## Proposition pour simplifier :
+      #sum_v = sum_v + dpois(x_vec[v] - k, lambda *  (1 - A[pav, v])) *
+                        #dbinom(k, x_vec[pav], A[pav, v])
+
     }
-    
+
     prod_vec[i] = sum_v
-    
   }
-  
+
   # output pdf
   dpois(x_vec[root_node], lambda) * prod(prod_vec)
-  
+
 }
 
 # Validation pdf_MPMRF ----
@@ -59,9 +62,6 @@ A = matrix(c(1, alpha12, 0, 0,
            nrow = 4,
            byrow = TRUE)
 
-xi = 0:10
-grid = expand.grid(xi, xi, xi, xi)
-sum(apply(grid, 1, function(x) pdf_MPMRF(A, 1, x, 1))) # somme à 1
-
-
-
+# xi = 0:10
+# grid = expand.grid(xi, xi, xi, xi)
+# sum(apply(grid, 1, function(x) pdf_MPMRF(A, 1, x, 1))) # somme à 1
