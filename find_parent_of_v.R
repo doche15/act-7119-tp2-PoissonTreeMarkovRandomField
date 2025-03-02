95541f9dbcb1167926594954aca5ee1156ade703
doche15 <dominik.chevalier.1@ulaval.ca>
2025-03-01 20:51:18 -0500

    Commenter exemples

--- find_parent_of_v.R
+++ find_parent_of_v.R
@@ -29,95 +29,95 @@ find_parent_of_v = function(A, v, root_node){
 
 }
 
-# Validation find_parent_of_v ----
-alpha12 = 0.2 ; alpha23 = 0.4; alpha24 = 0.7 # dépendances
-
-# Matrice adjacente (star)
-A = matrix(c(1, alpha12, 0, 0,
-             alpha12, 1, alpha23, alpha24,
-             0, alpha23, 1, 0,
-             0, alpha24, 0, 1),
-           nrow = 4,
-           byrow = TRUE)
-
-# pa(3) avec r = 1
-find_parent_of_v(A,
-                 v = 3,
-                 root_node = 1) # devrait être 2
-
-# pa(2) avec r = 3
-find_parent_of_v(A,
-                 v = 2,
-                 root_node = 3) # devrait être 3
-
-# pa(1) avec r = 1
-find_parent_of_v(A,
-                 v = 1,
-                 root_node = 1) # aucun
-
-# Matrice adjacente (ligne)
-alpha12 = 0.2 ; alpha23 = 0.4 # dépendances
-
-A = matrix(c(1, alpha12, 0,
-             alpha12, 1, alpha23,
-             0, alpha23, 1),
-           nrow = 3)
-
-# pa(3) avec r = 1
-find_parent_of_v(A,
-                 v = 3,
-                 root_node = 1) # devrait être 2
-
-# pa(2) avec r = 3
-find_parent_of_v(A,
-                 v = 2,
-                 root_node = 3) # devrait être 3
-
-# pa(1) avec r = 1
-find_parent_of_v(A,
-                 v = 1,
-                 root_node = 1) # aucun
-
-# Matrice adjacente (hybride)
-alpha12 = 0.2 ; alpha13 = 0.4 ; alpha34 = 0.7; alpha35 = 0.5# dépendances
-
-A = matrix(c(1, alpha12, alpha13, 0, 0,
-             alpha12, 1, 0, 0, 0,
-             alpha13, 0, 1, alpha34, alpha35,
-             0, 0, alpha34, 1, 0,
-             0, 0, alpha35, 0, 1),
-           nrow = 5)
-
-see_tree_graph(A,
-               root_node = 1,
-               titre = "Arbre hybride")
-
-# pa(3) avec r = 1
-find_parent_of_v(A,
-                 v = 3,
-                 root_node = 1) # devrait être 1
-
-# pa(1) avec r = 1
-find_parent_of_v(A,
-                 v = 1,
-                 root_node = 1) # aucun
-
-# pa(4) avec r = 1
-find_parent_of_v(A,
-                 v = 4,
-                 root_node = 1)
-
-# change root pour r = 3
-see_tree_graph(A,
-               root_node = 3,
-               titre = "Arbre hybride")
-
-# pa(4) avec r = 3
-find_parent_of_v(A,
-                 v = 2,
-                 root_node = 3) # devrai être 1
-
-# pa(4) avec r = 3
-find_parent_of_v(A,
-                 v = 4,
-                 root_node = 3) # devrai être 3
+# # Validation find_parent_of_v ----
+# alpha12 = 0.2 ; alpha23 = 0.4; alpha24 = 0.7 # dépendances
+#
+# # Matrice adjacente (star)
+# A = matrix(c(1, alpha12, 0, 0,
+#              alpha12, 1, alpha23, alpha24,
+#              0, alpha23, 1, 0,
+#              0, alpha24, 0, 1),
+#            nrow = 4,
+#            byrow = TRUE)
+#
+# # pa(3) avec r = 1
+# find_parent_of_v(A,
+#                  v = 3,
+#                  root_node = 1) # devrait être 2
+#
+# # pa(2) avec r = 3
+# find_parent_of_v(A,
+#                  v = 2,
+#                  root_node = 3) # devrait être 3
+#
+# # pa(1) avec r = 1
+# find_parent_of_v(A,
+#                  v = 1,
+#                  root_node = 1) # aucun
+#
+# # Matrice adjacente (ligne)
+# alpha12 = 0.2 ; alpha23 = 0.4 # dépendances
+#
+# A = matrix(c(1, alpha12, 0,
+#              alpha12, 1, alpha23,
+#              0, alpha23, 1),
+#            nrow = 3)
+#
+# # pa(3) avec r = 1
+# find_parent_of_v(A,
+#                  v = 3,
+#                  root_node = 1) # devrait être 2
+#
+# # pa(2) avec r = 3
+# find_parent_of_v(A,
+#                  v = 2,
+#                  root_node = 3) # devrait être 3
+#
+# # pa(1) avec r = 1
+# find_parent_of_v(A,
+#                  v = 1,
+#                  root_node = 1) # aucun
+#
+# # Matrice adjacente (hybride)
+# alpha12 = 0.2 ; alpha13 = 0.4 ; alpha34 = 0.7; alpha35 = 0.5# dépendances
+#
+# A = matrix(c(1, alpha12, alpha13, 0, 0,
+#              alpha12, 1, 0, 0, 0,
+#              alpha13, 0, 1, alpha34, alpha35,
+#              0, 0, alpha34, 1, 0,
+#              0, 0, alpha35, 0, 1),
+#            nrow = 5)
+#
+# # see_tree_graph(A,
+# #                root_node = 1,
+# #                titre = "Arbre hybride")
+#
+# # pa(3) avec r = 1
+# find_parent_of_v(A,
+#                  v = 3,
+#                  root_node = 1) # devrait être 1
+#
+# # pa(1) avec r = 1
+# find_parent_of_v(A,
+#                  v = 1,
+#                  root_node = 1) # aucun
+#
+# # pa(4) avec r = 1
+# find_parent_of_v(A,
+#                  v = 4,
+#                  root_node = 1)
+#
+# # change root pour r = 3
+# see_tree_graph(A,
+#                root_node = 3,
+#                titre = "Arbre hybride")
+#
+# # pa(4) avec r = 3
+# find_parent_of_v(A,
+#                  v = 2,
+#                  root_node = 3) # devrai être 1
+#
+# # pa(4) avec r = 3
+# find_parent_of_v(A,
+#                  v = 4,
+#                  root_node = 3) # devrai être 3
--- pdf_M.R
+++ pdf_M.R
@@ -6,69 +6,69 @@
 pdf_M = function(A, lambda){
   # A : matrice d'adjacence
   # lambda : paramètre des lois de Poisson
-  
+
   d = nrow(A)
-  
+
   nfft = 2^15
-  
+
   b = c(0, 1, rep(0, nfft - 2)) # fmp v.a. dégénérée à 1
-  
+
   phib = fft(b)
-  
+
   phiM = numeric(nfft)
-  
+
   k_domain = rev(seq(d)[-1])
-  
+
   for (l in seq(nfft)){
-    
+
     H = matrix(data = 1,
                nrow = d,
                ncol = d) # all-1 matrix
-    
+
     prod_vec = numeric(length(k_domain))
-    
+
     for (k in k_domain){
-      
+
       pi_k = min(which(A[k,] > 0)) # infimum (devrait jamais donner infini parce
       # que sinon ça veut dire qu'il y a un node indépendant de tous les autres)
-      
+
       h_k = phib[l] * prod(H[k,])
-      
+
       H[pi_k, k] = 1 - A[pi_k, k] + A[pi_k, k] * h_k # overwrite H
-      
-      prod_vec[k - 1] = exp(lambda * (1 - A[pi_k, k]) * 
+
+      prod_vec[k - 1] = exp(lambda * (1 - A[pi_k, k]) *
                               (h_k - 1))
-      
+
     }
-    
+
     h_1 = phib[l] * prod(H[1,])
-    
+
     phiM[l] = prod(prod_vec) * exp(lambda * (h_1 - 1)) # ajouter k = 1
-    
+
   }
-  
+
   # output pdf_M
   Re(fft(phiM, inverse = T))/nfft
-  
+
 }
-
-# Validation pdf_M ----
-alpha12 = 0.2 ; alpha23 = 0.4; alpha24 = 0.7 # dépendances
-
-# Matrice adjacente
-A = matrix(c(1, alpha12, 0, 0,
-             alpha12, 1, alpha23, alpha24,
-             0, alpha23, 1, 0,
-             0, alpha24, 0, 1),
-           nrow = 4,
-           byrow = TRUE)
-
-lambda = 1 # paramètre des lois de Poisson
-
-fM = pdf_M(A, lambda)
-sum(fM) # somme à 1
-
-m = seq(nfft) - 1
-esp_M = sum(m*fM) # changer les dépendances ne devrait pas avoir d'impact
-var_M = sum(m^2*fM) - esp_M^2 # augmenter la dépendance devrait augmenter la variance
-
+#
+# # Validation pdf_M ----
+# alpha12 = 0.2 ; alpha23 = 0.4; alpha24 = 0.7 # dépendances
+#
+# # Matrice adjacente
+# A = matrix(c(1, alpha12, 0, 0,
+#              alpha12, 1, alpha23, alpha24,
+#              0, alpha23, 1, 0,
+#              0, alpha24, 0, 1),
+#            nrow = 4,
+#            byrow = TRUE)
+#
+# lambda = 1 # paramètre des lois de Poisson
+#
+# fM = pdf_M(A, lambda)
+# sum(fM) # somme à 1
+#
+# m = seq(nfft) - 1
+# esp_M = sum(m*fM) # changer les dépendances ne devrait pas avoir d'impact
+# var_M = sum(m^2*fM) - esp_M^2 # augmenter la dépendance devrait augmenter la variance
+#
