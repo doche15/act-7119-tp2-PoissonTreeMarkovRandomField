cf77609dffce8d153210a0a7c809b1ba92ce4ca4
doche15 <dominik.chevalier.1@ulaval.ca>
2025-03-13 13:23:44 -0400

    Exemple de la bete

--- illustrations_numeriques/bete.R
+++ illustrations_numeriques/bete.R
@@ -9,12 +9,9 @@ library(RColorBrewer)
 
 ### Reproduction de l'exemple numérique (Section 8)
 
-lambda <- 1
+lambda <- 3
 
 #### Construction de la matrice d'adjacence
-beta <- c(0.3, 0.7, 0.9)
-
-d <- 50
 
 ## Arètes (selon figure 5 de l'article)
 # (1, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8),
@@ -55,50 +52,111 @@ for (i in 1:nrow(aretes))
 
 source("graph_A.R")
 
-see_tree_graph(A0.7, 1, titre = "Figure 5")
+see_tree_graph(A, 1, titre = "Figure 5")
 
 
 
 
-### pdf_M (algorithme 3)
+### Résultats
 source("pdf_M.R")
+source("Algorithm_4.R")
+source("sample_MPMRF.R")
 nfft <- 2^15
-fM0 <- dpois(0:(nfft - 1), lambda * d)
-fM0.3 <- pdf_M(A0.3, lambda)
-fM0.7 <- pdf_M(A0.7, lambda)
-fM0.9 <- pdf_M(A0.9, lambda)
+fM <- pdf_M(A, lambda)
+mm <- 0:(nfft - 1)
+nn <- 5e5
+set.seed(80934)
+realisations <- rMPMRF(nn, A, lambda)
+reaM <- rowSums(realisations)
+
+sum(mm * fM)
+sum(mm^2 * fM) - sum(mm * fM)^2
+mean(reaM)
+mean(reaM^2) - mean(reaM)^2
+
+cumsum(fM)[750]
+mean(reaM <= 749)
 
 
-df_FM <- data.frame(x = 0:(nfft - 1),
-                    "beta0" = fM0,
-                    "beta0.3" = fM0.3,
-                    "beta0.7" = fM0.7,
-                    "beta0.9" = fM0.9) %>% pivot_longer(cols = -x,
-                                                        names_to = "beta",
-                                                        values_to = "fM")
+(v0.9 <- min(which(cumsum(fM) >= 0.9)) - 1)
+sort(reaM)[0.9 * nn]
+
+sum(pmax(mm - v0.9, 0) * fM) / (1 - 0.9) + v0.9
+mean(sort(reaM)[(0.9 * nn):nn])
+
+alloc1 <- exp_alloc_Nv(A, 1, lambda, 1, nfft)
+alloc2 <- exp_alloc_Nv(A, 1, lambda, 2, nfft)
+alloc51 <- exp_alloc_Nv(A, 1, lambda, 51, nfft)
+
+contrib_tvar <- function(fM, alloc, lambda, kappa)
+{
+    FM <- cumsum(fM)
+    varr <- min(which(FM >= kappa)) - 1
+    aa <- lambda - sum(alloc[0:varr + 1]) + (FM[varr + 1] - kappa) /fM[varr + 1] *
+        alloc[varr + 1]
+
+    1 / (1 - kappa) * aa
+}
+
+contrib_tvar(fM, alloc1, lambda, 0.9)
+contrib_tvar(fM, alloc2, lambda, 0.9)
+contrib_tvar(fM, alloc51, lambda, 0.9)
+
+mean(realisations[, 1] * (reaM > 855)) / (1 - 0.9)
+mean(realisations[, 2] * (reaM > 855)) / (1 - 0.9)
+mean(realisations[, 51] * (reaM > 855)) / (1 - 0.9)
+
+df <- data.frame(x = 0:(nfft - 1),
+                    "fM" = fM)
 
 
 dark2 <- brewer.pal(8, "Dark2")
-couleurs <- c("M0" = dark2[1], "M0.3" = dark2[2],
-              "M0.7" = dark2[3],
-              "M0.9" = dark2[8])
 
-(ggFM <- ggplot(df_FM %>% filter(x <= 200), aes(x = x, y = fM, col = beta)) +
-    geom_segment(aes(xend = x, yend = 0), show.legend = TRUE) +
-    geom_point() +
-    labs(title = bquote("FMP de M selon les valeurs de" ~ beta),
+(ggFM <- ggplot(df %>% filter(x <= 1300), aes(x = x, y = fM)) +
+    #geom_segment(aes(xend = x, yend = 0), show.legend = TRUE, col = dark2[1]) +
+    geom_vline(xintercept = 855, color = dark2[2], linetype = "dashed", linewidth = 1) +
+    geom_vline(xintercept = 896.88, color = dark2[3], linetype = "dashed", linewidth = 1) +
+        annotate("text", x = 730, y = 0.006, label = expression(VaR[0.9](M)), parse = TRUE, hjust = 0) +
+        annotate("text", x = 910, y = 0.006, label = expression(TVaR[0.9](M)), parse = TRUE, hjust = 0) +
+    geom_point(col = dark2[1]) +
+    labs(title = bquote("FMP de M pour la bête"),
          x = expression(x),
          y = expression(p[M](x)),
          col = expression(beta)) +
-    theme_classic() +
-        scale_color_manual(values = setNames(couleurs, c("beta0",
-                                                         "beta0.3",
-                                                         "beta0.7",
-                                                         "beta0.9")),
-                           labels = c("beta0" = 0,
-                                                         "beta0.3" = 0.3,
-                                                         "beta0.7" = 0.7,
-                                                         "beta0.9" = 0.9)))
-ggsave("illustrations_numeriques/figs/repro_section8/fM.pdf",
+    theme_classic())
+
+ggsave("illustrations_numeriques/figs/bete/fMbete.pdf",
+       width = 9,
+       height = 4.8)
+
+
+df_alloc <- data.frame(x = 0:(nfft - 1),
+                       "alloc1" = alloc1 / fM,
+                       "alloc2" = alloc2 / fM,
+                       "alloc51" = alloc51 / fM) %>% pivot_longer(cols = -x,
+                                                                  names_to = "nu",
+                                                                  values_to = "alloc")
+
+dark2 <- brewer.pal(8, "Dark2")
+couleurs <- c("alloc1" = dark2[1],
+              "alloc2" = dark2[2],
+              "alloc51" = dark2[3])
+
+(ggFM <- ggplot(df_alloc %>% filter(x <= 1500, x > 350), aes(x = x, y = alloc, col = nu)) +
+        #geom_segment(aes(xend = x, yend = 0), show.legend = TRUE) +
+        geom_point() +
+        labs(title = bquote("Espérance conditionnelle de" ~ N[nu] ~ "sachant" ~  M == k),
+             x = expression(k),
+             y = bquote(E[N[nu] ~ "|" ~ M == k]),
+             col = expression(nu)) +
+        theme_classic() +
+        scale_color_manual(values = setNames(couleurs, c("alloc1",
+                                                         "alloc2",
+                                                         "alloc51")),
+                           labels = c("alloc1" = 1,
+                                      "alloc2" = 2,
+                                      "alloc51" = 51)))
+
+ggsave("illustrations_numeriques/figs/bete/enmbete.pdf",
        width = 9,
        height = 4.8)
